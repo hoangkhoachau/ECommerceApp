0084c4f4d95c924b81079bc0dc67eb1a06e611a5
Chau Hoang Khoa <21120481@student.hcmus.edu.vn>
2024-07-02 19:14:00 +0700

    Initial Commit

---
+++ Sources/MyStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStoreApp.swift
+//  MyStore
+//
+//  Created by Chau Hoang Khoa on 28/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
