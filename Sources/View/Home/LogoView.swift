aa7fb6d4e49fed547da53c1ed1a49d4fda526e9e
Chau Hoang Khoa <21120481@student.hcmus.edu.vn>
2024-07-04 15:07:57 +0700

    Create custom Navigation bar with animation in the logo and apply modifiers so that it fits in all the iOS devices.

---
+++ Sources/View/Home/LogoView.swift
@@ -0,0 +1,37 @@
+//
+//  LogoView.swift
+//  MyStore
+//
+//  Created by Chau Hoang Khoa on 29/06/2024.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        }  //: HSTACK
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
