7b9cd945dbe4fc239dfec20ecca254b52e755e57
Chau Hoang Khoa <21120481@student.hcmus.edu.vn>
2024-07-05 12:15:05 +0700

    Implement featured tab view images with adaptive carousel.

---
+++ Sources/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  MyStore
+//
+//  Created by Chau Hoang Khoa on 30/06/2024.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
---
+++ Sources/View/Home/FeaturedTabView.swift
@@ -0,0 +1,28 @@
+//
+//  FeaturedTabView.swift
+//  MyStore
+//
+//  Created by Chau Hoang Khoa on 30/06/2024.
+//
+
+import SwiftUI
+
+struct FeaturedTabView: View {
+    var body: some View {
+        TabView {
+            ForEach(players) { player in
+                FeaturedItemView(player: player)
+                    .padding(.top, 10)
+                    .padding(.horizontal, 15)
+            }  //: FOR LOOP
+        }  //: TAB VIEW
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+struct FeaturedTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedTabView()
+            .background(Color.gray)
+    }
+}
